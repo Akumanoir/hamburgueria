39aafd969374ceb50e0a4b3ab3edad5e00e328e0
Cristiano Reis <117485646+Akumanoir@users.noreply.github.com>
2023-11-18 16:11:28 -0300

    added dinamic heght to itempage.tsx main tag

--- src/pages/ItemPage.tsx
+++ src/pages/ItemPage.tsx
@@ -18,71 +18,79 @@ export function ItemPage() {
     setWidth(window.innerWidth);
   };
 
+  const mainHeight = () => {
+    if (width < 825) {
+    return { height: "calc(100dvh - 6.125rem)" };
+  }
+  }
+
   useEffect(() => {
     window.addEventListener("resize", updateWidth);
     return () => window.removeEventListener("resize", updateWidth);
   });
 
   return (
-    <main key={item.id} className="max-w-3xl m-auto">
-      <div>
-        <div className="relative w-full">
-          {width < 835 && (
-            <Link
-              to={`/menu`}
-              className="absolute right-0 m-4 text-white hover:text-green-400 transition duration-200"
-            >
-              <XCircle />
-            </Link>
-          )}
-          <img
-            src={item.image}
-            alt="image de hamburguer"
-            className="object-cover w-full h-full"
-          />
+    <main
+      key={item.id}
+      className="max-w-3xl m-auto flex flex-col"
+      style={mainHeight()}
+    >
+      <section className="relative w-full">
+        {width < 835 && (
+          <Link
+            to={`/menu`}
+            className="absolute right-0 m-4 text-white hover:text-green-400 transition duration-200"
+          >
+            <XCircle />
+          </Link>
+        )}
+        <img
+          src={item.image}
+          alt="image de hamburguer"
+          className="object-cover w-full h-full  max-h-[255px]"
+        />
+      </section>
+      <section className="p-3 flex flex-col gap-3 flex-grow">
+        <div className="flex justify-between">
+          <div className="">
+            <h2 className="font-bold text-lg">{item.name}</h2>
+          </div>
+          <div>
+            {width > 835 && (
+              <Link
+                to={`/menu`}
+                className="hover:text-green-400 transition duration-200"
+              >
+                <XCircle />
+              </Link>
+            )}
+          </div>
         </div>
-        <section className="p-3 grid gap-3">
-          <div className="flex justify-between">
-            <div className="">
-              <h2 className="font-bold text-lg">{item.name}</h2>
-            </div>
-            <div>
-              {width > 835 && (
-                <Link
-                  to={`/menu`}
-                  className="hover:text-green-400 transition duration-200"
-                >
-                  <XCircle />
-                </Link>
-              )}
-            </div>
-          </div>
-          <p>{item.description}</p>
-          {item.ingredients.length ? (
-            <ul className="list-disc list-inside">
-              {item.ingredients.map((ingredients: string, position: number) => (
-                <li key={position}>{ingredients}</li>
-              ))}
-            </ul>
-          ) : (
-            <p>Não disponível</p>
-          )}
-          <div className="grid gap-3">
-            <strong className="block">R${item.value}</strong>
-            <ConfirmButton
-              text="Adicionar ao carrinho"
-              onAction={() =>
-                itemsClass.createShoppingCart(
-                  item.id,
-                  item.image,
-                  item.name,
-                  item.value
-                )
-              }
-            />
-          </div>
-        </section>
-      </div>
+        <p>{item.description}</p>
+        {item.ingredients.length ? (
+          <ul className="list-disc list-inside">
+            {item.ingredients.map((ingredients: string, position: number) => (
+              <li key={position}>{ingredients}</li>
+            ))}
+          </ul>
+        ) : (
+          <p>Não disponível</p>
+        )}
+      </section>
+      <section className="p-3 grid gap-3">
+        <strong className="block">R${item.value}</strong>
+        <ConfirmButton
+          text="Adicionar ao carrinho"
+          onAction={() =>
+            itemsClass.createShoppingCart(
+              item.id,
+              item.image,
+              item.name,
+              item.value
+            )
+          }
+        />
+      </section>
     </main>
   );
 }
