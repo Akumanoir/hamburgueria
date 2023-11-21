98fda5f99b78c6c8ece7d60f3790b568c0269039
CRISTIANO GUIMARÃES REIS JÃNIOR <cristiano.junior@alunos.sc.senac.br>
2023-11-20 21:19:17 -0300

    added gap and padding to home page section tag

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -9,17 +9,22 @@ export function HomePage() {
 
   return (
       <section className="max-w-3xl m-auto">
-        <h2>Promoção</h2>
-        {itemsOnPromotion.map((items: any) => (
-            <Link to={`/menu/${items.id}`} key={items.name}>
-              <MenuButton
-                id = {items.id}
-                image = {items.image}
-                name = {items.name}
-                value = {items.value}
-              />
-            </Link>
-          ))}
+        <div className="p-3">
+          <h2>Promoção</h2>
+        </div>
+        <div className="grid gap-2">
+          {itemsOnPromotion.map((items: any) => (
+              <Link to={`/menu/${items.id}`} key={items.name}>
+                <MenuButton
+                  className="hover:border-green-400"
+                  id = {items.id}
+                  image = {items.image}
+                  name = {items.name}
+                  value = {items.value}
+                />
+              </Link>
+            ))}
+        </div>
       </section>
   )
 }
